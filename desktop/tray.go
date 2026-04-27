08eb1049d37a30f5136a8dd73299a905e9bef95a
Yan Zhang <yan.zhang@osip.cc>
2026-04-27 11:07:38 +0800

    feat: add system tray implementation for desktop proxy management

--- desktop/tray.go
+++ desktop/tray.go
@@ -25,34 +25,32 @@ func (a *App) onTrayReady() {
 	systray.AddSeparator()
 	mQuit := systray.AddMenuItem("退出", "完全退出 Kite Proxy")
 
-	go func() {
-		for {
-			select {
-			case <-mShow.ClickedCh:
-				go func() {
-					if a.ctx != nil {
-						runtime.WindowShow(a.ctx)
-						// 唤醒前端组件自动刷新数据
-						runtime.EventsEmit(a.ctx, "app:wakeup")
-					}
-				}()
-			case <-mHide.ClickedCh:
-				go func() {
-					if a.ctx != nil {
-						runtime.WindowHide(a.ctx)
-					}
-				}()
-			case <-mQuit.ClickedCh:
-				go func() {
-					systray.Quit()
-					if a.ctx != nil {
-						runtime.Quit(a.ctx)
-					}
-				}()
-				return
+	mShow.Click(func() {
+		go func() {
+			if a.ctx != nil {
+				runtime.WindowShow(a.ctx)
+				// 唤醒前端组件自动刷新数据
+				runtime.EventsEmit(a.ctx, "app:wakeup")
 			}
-		}
-	}()
+		}()
+	})
+
+	mHide.Click(func() {
+		go func() {
+			if a.ctx != nil {
+				runtime.WindowHide(a.ctx)
+			}
+		}()
+	})
+
+	mQuit.Click(func() {
+		go func() {
+			systray.Quit()
+			if a.ctx != nil {
+				runtime.Quit(a.ctx)
+			}
+		}()
+	})
 }
 
 // onTrayExit 托盘退出回调
